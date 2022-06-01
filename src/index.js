c684c1c2a2fe8ccd265febfe503fab322f46de05
Serhii Lebid <lebids24@gmail.com>
2022-06-01 23:12:03 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles/index.scss";
+
+const userStack = {
+   language: 'JavaScript',
+   framework: 'Angular'
+};
+
+const user = {
+   name: 'Vitalij',
+   age: '37',
+   ...userStack
+};
+
+console.log(user);
+console.log("hello!!");
