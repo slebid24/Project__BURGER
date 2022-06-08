276cef69244a7c82c0df36326704993ff3d11146
Serhii Lebid <lebids24@gmail.com>
2022-06-08 21:54:43 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import "./styles/index.scss";
 import "../src/module/main";
-import asss from "../src/module/main";
 
-const userStack = {
-   language: 'JavaScript',
-   framework: 'Angular'
-};
+window.addEventListener("DOMContentLoaded", () => {
 
-const user = {
-   name: 'Vitalij',
-   age: '37',
-   ...userStack
-};
+// Свайп 
 
-console.log(user);
-console.log("hello!!");
 
-asss();
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- src/module/main.js
+++
@@ -1,3 +0,0 @@
-export default function asss() {
-   console.log("HI");
-}
