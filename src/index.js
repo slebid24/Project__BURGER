efd36ad77acae087e4f8049b612cbd3cbed6f4d8
Serhii Lebid <lebids24@gmail.com>
2022-06-09 15:03:19 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ window.addEventListener("DOMContentLoaded", () => {
 // Свайп 
 
 
-
+const firstPageBtn = document.querySelector("#first-page-btn");
+const secondPageBtn = document.querySelector("#second-page-btn");
+const wrapper = document.querySelector(".main__wrapper");
+const field = document.querySelector(".main__inner");
+const pages = document.querySelectorAll(".")
 
 
 
