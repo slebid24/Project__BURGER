bc2e6185022e448a73f8ed9c0c8fa6a0508ecdd0
Serhii Lebid <lebids24@gmail.com>
2022-06-10 20:16:02 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -10,8 +10,23 @@ const firstPageBtn = document.querySelector("#first-page-btn");
 const secondPageBtn = document.querySelector("#second-page-btn");
 const wrapper = document.querySelector(".main__wrapper");
 const field = document.querySelector(".main__inner");
-const pages = document.querySelectorAll(".")
+const pages = document.querySelectorAll(".forslide");
 
+const width = window.getComputedStyle(wrapper).width;
+
+
+
+let slideIndex = 1;
+let offset = 0;
+field.style.width = 100 * pages.length + "%";
+field.style.display = "flex";
+field.style.transition = "0.5s all";
+wrapper.style.overflow = "hidden";
+
+   // 
+   pages.forEach(slide => {
+      slide.style.width = width;
+   });
 
 
 
