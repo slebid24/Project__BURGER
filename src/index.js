84b46a3f31fc2cb7724c66fb76807d8ef0fc4ef1
Serhii Lebid <lebids24@gmail.com>
2022-06-12 21:11:55 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,31 +2,147 @@ import "./styles/index.scss";
 import "../src/module/main";
 
 window.addEventListener("DOMContentLoaded", () => {
-
-// Свайп 
-
-
-const firstPageBtn = document.querySelector("#first-page-btn");
-const secondPageBtn = document.querySelector("#second-page-btn");
-const wrapper = document.querySelector(".main__wrapper");
-const field = document.querySelector(".main__inner");
-const pages = document.querySelectorAll(".forslide");
-
-const width = window.getComputedStyle(wrapper).width;
+   document.body.style.overflow = "hidden";
 
 
+   // Свайп 
+   const firstPageBtn = document.querySelector("#first-page-btn");
+   const secondPageBtn = document.querySelector("#second-page-btn");
+   const wrapper = document.querySelector(".main__wrapper");
+   const field = document.querySelector(".main__inner");
+   const pages = document.querySelectorAll(".forslide");
+   const page1 = document.querySelector(".main__inner-pg1");
+   const page2 = document.querySelector(".main__inner-pg2");
+   const mainBtn = document.querySelector(".main__btn");
+   const topBtns = document.querySelectorAll(".header__page-changer");
 
-let slideIndex = 1;
-let offset = 0;
-field.style.width = 100 * pages.length + "%";
-field.style.display = "flex";
-field.style.transition = "0.5s all";
-wrapper.style.overflow = "hidden";
+   const width = window.getComputedStyle(wrapper).width;
+   const widthAfterSwipe = window.getComputedStyle(page1).width;
+   console.log(widthAfterSwipe);
 
-   // 
+
+   let offset = 0;
+   field.style.width = 100 * pages.length + "vw";
+   field.style.display = "flex";
+   field.style.transition = "0.8s all";
+
+
    pages.forEach(slide => {
       slide.style.width = width;
    });
+
+
+   function deleteNotDigits(str) {
+      return +str.slice(0, str.length - 2);
+   }
+
+   page2.style.display = "none";
+   firstPageBtn.classList.add("header__page-changer--active");
+
+
+   function swipeToSecondPage() {
+      if (offset == 0) {
+         offset += deleteNotDigits(width);
+      } else {
+         return;
+      }
+      field.style.transform = `translateX(-${offset}px)`;
+      page1.style.display = "none";
+      page2.style.display = "flex";
+      page2.style.marginLeft = widthAfterSwipe;
+      secondPageBtn.classList.add("header__page-changer--active");
+      firstPageBtn.classList.remove("header__page-changer--active");
+   }
+
+   function swipeToFirstPage() {
+      if (offset > 0) {
+         offset -= deleteNotDigits(width);
+      } else {
+         return;
+      }
+      field.style.transform = `translateX(-${offset}px)`;
+      page1.style.display = "flex";
+      page2.style.display = "none";
+      page2.style.marginLeft = widthAfterSwipe;
+      firstPageBtn.classList.add("header__page-changer--active");
+      secondPageBtn.classList.remove("header__page-changer--active");
+   }
+
+
+
+   secondPageBtn.addEventListener("click", swipeToSecondPage);
+   mainBtn.addEventListener("click", swipeToSecondPage);
+   firstPageBtn.addEventListener("click", swipeToFirstPage);
+
+   // Ефект меню
+   topBtns.forEach((elem) => {
+
+      elem.onmouseenter =
+         elem.onmouseleave = (e) => {
+
+            const tolerance = 10;
+
+            const left = 0;
+            const right = elem.clientWidth;
+
+            let x = e.pageX - elem.offsetLeft;
+
+            if (x - tolerance < left) {
+               x = left;
+            }
+            if (x + tolerance > right) {
+               x = right;
+            }
+
+            elem.style.setProperty('--x', `${ x }px`);
+
+         };
+   });
+   //2 page
+   ////////////////////////////////////////////////////
+   
+   let generalObj = {
+      src: [],
+      alt: [],
+      title: [],
+      pieces: [],
+      price: [],
+      time: [],
+      kcal: []
+   };
+
+   function buildBurger() {
+      class Ingradiet {
+         constructor(src, alt, title, btnMinus, btnPlus, pieces, price, time, kcal) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.btnMinus = btnMinus;
+            this.btnMinus = btnPlus;
+            this.pieces = pieces;
+            this.price = price;
+            this.time = time;
+            this.kcal = kcal;
+         }
+      }
+      
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
